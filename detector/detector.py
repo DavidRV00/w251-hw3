af35523b1d87f51560524451259582bb2d315cb4
David Vaughan <dvdrv00@gmail.com>
2020-02-10 19:12:18 -0800

    Begin detector

---
+++ detector/detector.py
@@ -0,0 +1,15 @@
+import numpy as np
+import cv2 as cv
+face_cascade = cv.CascadeClassifier('/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml')
+
+# gray here is the gray frame you will be getting from a camera
+gray = cv.cvtColor(gray, cv.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+for (x,y,w,h) in faces:
+    # your logic goes here; for instance
+    # cut out face from the frame..
+    # rc,png = cv2.imencode('.png', face)
+    # msg = png.tobytes()
+    # ...
+    print('Here\'s one.')
+print('Done.')
