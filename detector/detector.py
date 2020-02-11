961ef677aee5886318091aad3bf2338405419caa
David Vaughan <dvdrv00@gmail.com>
2020-02-10 20:39:36 -0800

    Detect faces from webcam

--- detector/detector.py
+++ detector/detector.py
@@ -1,15 +1,20 @@
-import numpy as np
 import cv2 as cv
+import sys
+
 face_cascade = cv.CascadeClassifier('/usr/share/opencv4/haarcascades/haarcascade_frontalface_default.xml')
 
-# gray here is the gray frame you will be getting from a camera
-gray = cv.cvtColor(gray, cv.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-for (x,y,w,h) in faces:
-    # your logic goes here; for instance
-    # cut out face from the frame..
-    # rc,png = cv2.imencode('.png', face)
-    # msg = png.tobytes()
-    # ...
-    print('Here\'s one.')
+cap = cv.VideoCapture(1)
+while(True):
+    ret,frame = cap.read()
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+    for (x,y,w,h) in faces:
+        print('Here\'s one.')
+
+        face = gray[y:y+h, x:x+w]
+        # cv.imwrite('face.png', face)
+
+        _,png = cv.imencode('.png', face)
+        msg = png.tobytes()
+
 print('Done.')
